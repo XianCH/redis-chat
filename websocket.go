40ce0627bd6feb81693f0cede3c1e7775a65bb24
XianCH <1160698903@qq.com>
2024-03-14 22:19:29 +0800

    2024/3/14:fix websocket

--- websocket.go
+++ websocket.go
@@ -1,36 +1,51 @@
 package redischat
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"net/http"
+	"github.com/redis/go-redis/v9"
 )
 
-var upGrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
+// websocket 升级
 func RunSocket(w http.ResponseWriter, r *http.Request) {
-	user := r.Header.Get("user")
-	if user == "" {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		log.Println("user name is empty")
 		return
 	}
-	ws, err := upGrader.Upgrade(w, r, nil)
+
+	//升级服务
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return
 	}
-	id := string(uuid.New())
-	user := &User{
-        ID:id,
-		name: user,
-	}
-	client := &Client{
-		Send: make(chan []byte),
-	}
-
-	MyServer.Register <- client
+	//create new client
+	uuid := uuid.New()
+	user := NewUser(uuid.String(), username)
+	client := NewClient(conn, *user)
+	MyServer.register <- client
 	go client.Read()
-	go client.write()
+	go client.Write()
+	//todo：从redis中获取消息
+	message, err := rdc.LRange(ctx, "group_chat_message", 0, -1).Result()
+	if err != nil && err != redis.Nil {
+		log.Printf("redis LRange error :%v", err)
+		return
+	}
+	for _, msg := range message {
+		data := []byte(msg)
+		err := conn.WriteMessage(websocket.TextMessage, data)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
